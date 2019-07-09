7ce226fb890edfbc3b68fe1781bbe021e515d2d0
Alexandre Lotte <alexlotte16@gmail.com>
2019-07-09 12:22:59 -0400

    rm pokepages

--- render-pages.js
+++ render-pages.js
@@ -12,13 +12,13 @@ const renderPages = require('hyperstatic/src/renderPages');
     '/books'
   ]
 
-  // Get a list of pokemon pages
-  const pokemonPages = await fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
-    .then(response => response.json())
-    .then(data => data.pokemon.map(pokemon => `/pokedex/${pokemon.id}`))
+  // // Get a list of pokemon pages
+  // const pokemonPages = await fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
+  //   .then(response => response.json())
+  //   .then(data => data.pokemon.map(pokemon => `/pokedex/${pokemon.id}`))
 
-  // Add pokemons pages to urls
-  pages = pages.concat(pokemonPages)
+  // // Add pokemons pages to urls
+  // pages = pages.concat(pokemonPages)
 
   renderPages(pages)
     .then(() => {
