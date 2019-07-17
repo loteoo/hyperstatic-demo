130ee3e9ebb19e4d7e472ed7b561219ff615b5dd
Alexandre Lotte <alexlotte16@gmail.com>
2019-07-17 17:31:46 -0400

    upgrade

--- src/app/pages/Apod/index.jsx
+++ src/app/pages/Apod/index.jsx
@@ -1,3 +1,4 @@
+import { NoPrerender } from 'hyperstatic'
 import { htmlToVdom } from 'hyperstatic/src/htmlToVdom'
 import description from './description.md'
 import { Http, preloadImage } from '../../../utils'
@@ -6,16 +7,18 @@ import { HandleError } from '../../actions'
 export default (state) => (
   <div>
     {htmlToVdom(description)}
-    {state.apod
-      ? (
-        <div class="viewer">
-          <h1>{state.apod.title}</h1>
-          <img src={state.apod.url} alt={state.apod.title} />
-          <p>{state.apod.explanation}</p>
-        </div>
-      )
-      : <p>Waiting for <code>api.nasa.gov</code>...</p>
-    }
+    <NoPrerender>
+      {state.apod
+        ? (
+          <div class="viewer">
+            <h1>{state.apod.title}</h1>
+            <img src={state.apod.url} alt={state.apod.title} />
+            <p>{state.apod.explanation}</p>
+          </div>
+        )
+        : <p>Waiting for <code>api.nasa.gov</code>...</p>
+      }
+    </NoPrerender>
   </div>
 )
 
